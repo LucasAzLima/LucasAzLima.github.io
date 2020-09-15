4747b47d34a523da6762d5547c2773f8012a10cd
Lucas lima <lucas.a.lima13@outlookcom>
2020-09-15 07:50:32 -0300

    Updates

---
+++ precache-manifest.adca60870df931d52b9cb9bfda5066ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353adf912abe27ece9ac9a85eef81289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9f06463d07c173a5dca",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "89a8bca63db5abf4aa71",
+    "url": "/static/js/2.43d1d294.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43d1d294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f06463d07c173a5dca",
+    "url": "/static/js/main.bf6bd0fe.chunk.js"
+  },
+  {
+    "revision": "ff6cb6bdd2e3fcebf3c2",
+    "url": "/static/js/runtime-main.3635901f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
